612eec9210de66ce53547be0968eba2dc1349f62
Andrey Pakhomov <pahomeathome@gmail.com>
2022-06-06 02:11:19 +0300

    change color made

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,30 @@
+const startButton = document.querySelector(`[data-start]`)
+const stopButton = document.querySelector(`[data-stop]`)
+const bodyEl = document.querySelector(`body`);
 
+startButton.addEventListener(`click`, onStartButtonClick)
+stopButton.addEventListener(`click`, onStopButtonClick)
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+function changeBodyBackgroundColor() {
+    return bodyEl.style.backgroundColor = getRandomHexColor();
+}
+
+
+function onStartButtonClick(event) {
+
+    const intervalID = setInterval(changeBodyBackgroundColor, 1000)
+
+    startButton.disabled = true;
+}
+
+
+function onStopButtonClick(event) {
+
+    const intervalID = setInterval(getRandomHexColor(), 1000)
+
+    startButton.disabled = true;
+}
